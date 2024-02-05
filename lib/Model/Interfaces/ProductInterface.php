9b7e7a620d549737e681dd7da8947969d5d58e6c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-02-05 11:13:34 +0000

    Add More Fields into Product Model.

--- lib/Model/Interfaces/ProductInterface.php
+++ lib/Model/Interfaces/ProductInterface.php
@@ -1,10 +1,16 @@
 <?php namespace Vankosoft\CatalogBundle\Model\Interfaces;
 
+use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Resource\Model\ResourceInterface;
 use Vankosoft\PaymentBundle\Model\Interfaces\PayableObjectInterface;
 use Vankosoft\ApplicationBundle\Model\Interfaces\TaxonLeafInterface;
 
 interface ProductInterface extends ResourceInterface, PayableObjectInterface, TaxonLeafInterface
 {
-    
+    public function isPublished(): bool;
+    public function getInStock(): int;
+    public function getSlug(): ?string;
+    public function getDescription(): ?string;
+    public function getCategories(): Collection;
+    public function getPictures(): Collection;
 }
