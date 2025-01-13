ab2d6c45609866c143cb736b59d18251fc9fe2b6
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-01-13 07:11:10 +0000

    Create Custom Route to Get Product Form on Loacale DropDown Change.

--- lib/Controller/ProductExtController.php
+++ lib/Controller/ProductExtController.php
@@ -6,7 +6,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Form\FormTypeInterface;
 use Doctrine\Persistence\ManagerRegistry;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
+//use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
 use Vankosoft\CatalogBundle\Form\ProductForm;
 
 class ProductExtController extends AbstractController
