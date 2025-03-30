c37850bba17464435d0ed37b915e110d4c604817
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-03-30 13:18:22 +0000

    Remove Active Flag on PricingPlanSubscription Model.

--- lib/Model/Interfaces/PricingPlanSubscriptionInterface.php
+++ lib/Model/Interfaces/PricingPlanSubscriptionInterface.php
@@ -9,7 +9,6 @@ interface PricingPlanSubscriptionInterface extends ResourceInterface, Subscripti
 {
     public function getPricingPlan(): PricingPlanInterface;
     public function isPaid(): bool;
-    public function isActive(): bool;
     public function getGatewayAttributes(): array;
     public function isRecurringPayment(): bool;
     public function getRecurringPayment(): bool;
