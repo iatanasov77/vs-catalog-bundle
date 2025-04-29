d5051304c4c900e89f02edef2479641107d9638d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-04-29 06:01:07 +0000

    Add Pricing Plan Attribute Field into the Form.

--- lib/Form/Type/GatewayAttributeType.php
+++ lib/Form/Type/GatewayAttributeType.php
@@ -4,6 +4,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Vankosoft\CatalogBundle\Component\Product;
 
 class GatewayAttributeType extends AbstractType
 {
