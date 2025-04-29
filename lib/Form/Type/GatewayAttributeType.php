a757b93e2b58173683573ccea07ad0b43a4ca7a4
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-04-29 05:34:37 +0000

    Add Pricing Plan Attribute Field into the Form.

--- lib/Form/Type/GatewayAttributeType.php
+++ lib/Form/Type/GatewayAttributeType.php
@@ -4,22 +4,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Vankosoft\CatalogBundle\Component\Product;
 
 class GatewayAttributeType extends AbstractType
 {
-    public function buildForm( FormBuilderInterface $builder, array $options )
+    public function buildForm( FormBuilderInterface $builder, array $options ): void
     {
         $builder
             ->add( 'name', ChoiceType::class, [
                 'required'              => false,
                 'choices'               => \array_flip( Product::PRICING_PLAN_ATTRIBUTE_KEYS ),
-                'placeholder'           => 'vankosoft_org.form.project.attribute_placeholder',
-                'translation_domain'    => 'VankoSoftOrg',
+                'placeholder'           => 'vs_catalog.form.pricing_plan.gateway_attribute_key_placeholder',
+                'translation_domain'    => 'VSCatalogBundle',
             ])
             ->add( 'value', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'vs_catalog.form.pricing_plan.gateway_attribute_value_placeholder'
+                ],
                 'required'              => false,
-                'translation_domain'    => 'VankoSoftOrg',
+                'translation_domain'    => 'VSCatalogBundle',
             ])
         ;
     }
