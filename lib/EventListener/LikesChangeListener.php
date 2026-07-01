6b3c6f52b5c5dd597da9dcc96def0a24652093c0
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-07-01 08:48:17 +0000

    Create Likes Change Listener and Updater for Average Rating.

---
+++ lib/EventListener/LikesChangeListener.php
@@ -0,0 +1,44 @@
+<?php namespace Vankosoft\CatalogBundle\EventListener;
+
+use Doctrine\ORM\Event\PostRemoveEventArgs;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Sylius\Component\Review\Model\ReviewableInterface;
+use Vankosoft\CatalogBundle\Component\Review\ReviewableRatingUpdaterInterface;
+
+class LikesChangeListener
+{
+    /** @var ReviewableRatingUpdaterInterface */
+    private $averageRatingUpdater;
+    
+    public function __construct(
+        ReviewableRatingUpdaterInterface $averageRatingUpdater
+    ) {
+        $this->averageRatingUpdater = $averageRatingUpdater;
+    }
+    
+    public function postPersist( LifecycleEventArgs $args )
+    {
+        $this->recalculateSubjectRating( $args );
+    }
+    
+    public function postUpdate( LifecycleEventArgs $args )
+    {
+        $this->recalculateSubjectRating( $args );
+    }
+    
+    public function postRemove( LifecycleEventArgs $args )
+    {
+        $this->recalculateSubjectRating( $args );
+    }
+    
+    public function recalculateSubjectRating( LifecycleEventArgs $args ): void
+    {
+        $subject = $args->getObject();
+        
+        if ( ! $subject instanceof ReviewableInterface ) {
+            return;
+        }
+        
+        $this->averageRatingUpdater->update( $subject );
+    }
+}
